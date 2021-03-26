aa3942d6681145d136b53b4c18d8769337ee9687
Hugh <hughyuen01@gmail.com>
2021-03-26 00:32:44 +0000

    route update

--- solution/routes/index.js
+++ solution/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Image Browsing' });
 });
 
+//router.post('/', ****controller.js****);
+
+
+
+router.get('/room', function(req, res, next) {
+  res.render('room', { title: 'Room' });
+});
+
+//router.post('/room', ****controller.js****);
+
+
+
+router.get('/image', function(req, res, next) {
+  res.render('image', { title: 'Image' });
+});
+
+//router.post('/image', ****controller.js****);
+
+
+
+router.get('/upload', function(req, res, next) {
+  res.render('upload', { title: 'Upload' });
+});
+
+//router.post('/upload', ****controller.js****);
+
+
+
 module.exports = router;
